679a04523ccb2db055f5bb0141d8d3b37b89f6c4
bristow17 <bristowgrorge17@gmail.com>
2024-06-13 05:04:10 +0530

    user add api

--- App.js
+++ App.js
@@ -11,6 +11,29 @@ app.use(express.json())
 mongoose.connect("mongodb+srv://bristow17:bristow1904@cluster0.vn8j7z4.mongodb.net/usersdb?retryWrites=true&w=majority&appName=Cluster0")
 
 
+app.post("/add",(req,res)=>{
+
+    let input=req.body
+    let user=new usermodel(input)
+    user.save()
+    res.json({"status":"success"})
+    
+})
+
+app.get("/viewall",(req,res)=>{
+
+    usermodel.find().then(
+        
+        (data)=>{
+            res.json(data)
+        }
+    ).catch(
+        (error)=>{
+            res.json(error)
+        }
+    )
+}
+)
 app.listen(8080,()=>(
     console.log("server started")
 ))
